e98c34b55f9bb5f3c0c7131aa0528966c6e927de
Veer Bawa <vbawa89@gmail.com>
2024-11-20 03:02:58 -0500

    update run_local

--- osm_proxy/run_local.py
+++ osm_proxy/run_local.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
+"""
+Provide a string as the first and only argument to this script. This string
+will need to be sent in every request as the `x-api-key` header.
+"""
+
 import os
 import shlex
 import subprocess
+import sys
 
-from typing import Dict
+def _call(cmd: str, env = None) -> str:
+    return subprocess.check_output(shlex.split(cmd), env=env).decode("utf-8")
 
-def _call(cmd: str) -> str:
-    return subprocess.check_output(shlex.split(cmd)).decode("utf-8")
-
-def main():
+def main(api_key: str) -> None:
     print("Running Tern OSM server locally")
     # cd to script's dir
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
@@ -17,9 +21,8 @@ def main():
     # Install dependencies
     _call("npm install")
     _call("redis-server")
-    _call("npx tsc")
-    _call("node dist/index.js")
-
+    _call("npx ts-node src/index.ts", env={"TERN_API_KEY": api_key})
 
 if __name__ == "__main__":
-    main()
+    (api_key,) = sys.argv[1:]
+    main(api_key)
