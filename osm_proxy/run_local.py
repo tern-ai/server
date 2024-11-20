f2ac68ffc34f8a41f824502ab690a1598cc646cf
Veer Bawa <vbawa89@gmail.com>
2024-11-20 03:08:16 -0500

    doc update

--- osm_proxy/run_local.py
+++ osm_proxy/run_local.py
@@ -3,7 +3,12 @@
 """
 Provide a string as the first and only argument to this script. This string
 will need to be sent in every request as the `x-api-key` header.
+
+You should have a Redis server running on your machine at port 6379
 """
+
+# TODO: Proper environment setup for redis-server. Will probably just move this
+# into a docker container
 
 import os
 import shlex
@@ -17,10 +22,7 @@ def main(api_key: str) -> None:
     print("Running Tern OSM server locally")
     # cd to script's dir
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
-    # TODO: Maybe needs env vars set up
-    # Install dependencies
     _call("npm install")
-    _call("redis-server")
     _call("npx ts-node src/index.ts", env={"TERN_API_KEY": api_key})
 
 if __name__ == "__main__":
